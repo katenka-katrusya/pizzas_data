48b0efdbbb12eee12d97ade185265b54140ccca8
SEkaterina <katerina-kozyrenko@yandex.ru>
2024-09-10 09:46:34 +0300

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 server.use(cors());
 server.use(middlewares);
